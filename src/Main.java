33d90ea0b55f68b348e7c781c89ef177481a15b8
EgorovaIrina <Irina1978@yandex.ru>
2022-12-12 08:27:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        SalesManager salesManager = new SalesManager(new int[] {4, 6, 9});
+        System.out.println(salesManager.max());
+        System.out.println(salesManager.min());
+    }
+}
